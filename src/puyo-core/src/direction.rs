60a434f69c227b762e13f71fe961e6bde2379814
whitetac <9902abcd@gmail.com>
2023-09-10 06:28:49 +0900

    direction 추가

---
+++ src/puyo-core/src/direction.rs
@@ -0,0 +1,29 @@
+pub mod direction {
+    #[derive(Debug, PartialEq, Eq)]
+    pub enum Type {
+        UP,
+        RIGHT,
+        DOWN,
+        LEFT,
+    }
+
+    pub const COUNT: u32 = 4;
+
+    pub fn get_offset_x(direction: Type) -> i8 {
+        match direction {
+            Type::UP => 0,
+            Type::RIGHT => 1,
+            Type::DOWN => 0,
+            Type::LEFT => -1,
+        }
+    }
+
+    pub fn get_offset_y(direction: Type) -> i8 {
+        match direction {
+            Type::UP => 1,
+            Type::RIGHT => 0,
+            Type::DOWN => -1,
+            Type::LEFT => 0,
+        }
+    }
+}
