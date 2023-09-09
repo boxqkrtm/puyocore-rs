c004157a5025cea14ce92b5ba5941875efab9f84
whitetac <9902abcd@gmail.com>
2023-09-10 05:48:43 +0900

    puyo-core 프로젝트 시작

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tuyotuyo-rs
+
+4color 3next 6*13 puyo puyo only ai 
---
+++ src/puyo-core/src/cell.rs
@@ -0,0 +1,51 @@
+pub mod cell {
+    #[derive(Debug, PartialEq, Eq, Copy, Clone)]
+    pub enum Type {
+        RED,
+        YELLOW,
+        GREEN,
+        BLUE,
+        GARBAGE,
+        NONE,
+    }
+
+    pub fn to_char(cell: Type) -> char {
+        match cell {
+            Type::RED => 'R',
+            Type::YELLOW => 'Y',
+            Type::GREEN => 'G',
+            Type::BLUE => 'B',
+            Type::GARBAGE => '#',
+            Type::NONE => '.',
+        }
+    }
+
+    pub fn from_char(c: char) -> Type {
+        match c {
+            'R' => Type::RED,
+            'Y' => Type::YELLOW,
+            'G' => Type::GREEN,
+            'B' => Type::BLUE,
+            '#' => Type::GARBAGE,
+            _ => Type::NONE,
+        }
+    }
+}
+#[test]
+fn test() {
+    //from char test
+    assert_eq!(cell::Type::NONE, cell::from_char('.'));
+    assert_eq!(cell::Type::RED, cell::from_char('R'));
+    assert_eq!(cell::Type::GREEN, cell::from_char('G'));
+    assert_eq!(cell::Type::BLUE, cell::from_char('B'));
+    assert_eq!(cell::Type::YELLOW, cell::from_char('Y'));
+    assert_eq!(cell::Type::GARBAGE, cell::from_char('#'));
+
+    //to char test
+    assert_eq!(cell::to_char(cell::Type::NONE), '.');
+    assert_eq!(cell::to_char(cell::Type::RED), 'R');
+    assert_eq!(cell::to_char(cell::Type::GREEN), 'G');
+    assert_eq!(cell::to_char(cell::Type::BLUE), 'B');
+    assert_eq!(cell::to_char(cell::Type::YELLOW), 'Y');
+    assert_eq!(cell::to_char(cell::Type::GARBAGE), '#');
+}
