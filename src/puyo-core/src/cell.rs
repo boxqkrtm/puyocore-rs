e68c6db122e75a10bdbf2321d7ef8c4688d858ec
whitetac <9902abcd@gmail.com>
2023-09-10 07:14:02 +0900

    rgb 순서변경

--- src/puyo-core/src/cell.rs
+++ src/puyo-core/src/cell.rs
@@ -15,9 +15,9 @@ impl Cell {
     pub fn to_char(&self) -> char {
         match self.element {
             CellType::RED => 'R',
-            CellType::YELLOW => 'Y',
             CellType::GREEN => 'G',
             CellType::BLUE => 'B',
+            CellType::YELLOW => 'Y',
             CellType::GARBAGE => '#',
             CellType::NONE => '.',
         }
@@ -26,9 +26,9 @@ impl Cell {
 pub fn from_char(c: char) -> Cell {
     let cell_type = match c {
         'R' => CellType::RED,
-        'Y' => CellType::YELLOW,
         'G' => CellType::GREEN,
         'B' => CellType::BLUE,
+        'Y' => CellType::YELLOW,
         '#' => CellType::GARBAGE,
         _ => CellType::NONE,
     };
