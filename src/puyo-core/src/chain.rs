206bc0403ac3eeeb4f8863308568dc6e5c834303
whitetac <9902abcd@gmail.com>
2023-09-18 08:34:55 +0000

    optimize +16%

--- src/puyo-core/src/chain.rs
+++ src/puyo-core/src/chain.rs
@@ -1,16 +1,18 @@
 use crate::{field::Field, cell};
 
 pub struct Score {
-    pub count: i32,
-    pub score: i32,
+    pub count: u32,
+    pub score: u32,
 }
 const COLOR_BONUS: [u32; 6] = [0, 0, 3, 6, 12, 24];
 const GROUP_BONUS: [u32; 12] = [0, 0, 0, 0, 0, 2, 3, 4, 5, 6, 7, 10];
+const MAX_GROUP_BONUS: usize = 11;
+const MAX_CHAIN_POWER: u32 = 999;
 const POWER: [u32; 19] = [
     0, 8, 16, 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, 480, 512,
 ];
 pub fn get_score(mut mask: Vec<Field>) -> Score {
-    let mut result = Score { count: mask.len() as i32, score: 0 };
+    let mut result = Score { count: mask.len() as u32, score: 0 };
 
     for index in 0..mask.len() {
         let pop_count = mask[index].get_count();
@@ -18,13 +20,10 @@ pub fn get_score(mut mask: Vec<Field>) -> Score {
         let chain_power = POWER[index];
 
         let mut color = 0;
-        for cell in 0..cell::COUNT - 1 {
-            color += if mask[index].data[cell].get_count() > 0 {
-                1   
-            }else{
-                0
-            };
+        for cell in &mask[index].data[..cell::COUNT - 1] {
+            color += if cell.get_count() > 0 { 1 } else { 0 };
         }
+        
         let bonus_color = COLOR_BONUS[color as usize];
 
         let mut group_bonus = 0;
@@ -35,7 +34,8 @@ pub fn get_score(mut mask: Vec<Field>) -> Score {
                 group_bonus += GROUP_BONUS[std::cmp::min(11, group.get_count() as usize)];
             }
         }
-        result.score += pop_count as i32 * 10 * std::cmp::max(1, std::cmp::min(999, chain_power + bonus_color + group_bonus)) as i32;
+        
+        result.score += pop_count as u32 * 10 * std::cmp::max(1, std::cmp::min(MAX_CHAIN_POWER, chain_power + bonus_color + group_bonus)) as u32;
     }
     result
 }
