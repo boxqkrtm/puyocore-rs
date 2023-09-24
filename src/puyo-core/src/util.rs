1fcc3f0bc46899e1a572749dfb012d6c404520aa
whitetac <9902abcd@gmail.com>
2023-09-24 23:02:42 +0900

    remove pext32_emu

--- src/puyo-core/src/util.rs
+++ src/puyo-core/src/util.rs
@@ -52,68 +52,6 @@ pub fn pext15_emu(v: u16, m: u16) -> u16 {
     }
 }
 
-pub fn pext32_emu(v: u32, m: u32) -> u32 {
-    unsafe {
-        let v_u32: u32 = v;
-        let m_u32: u32 = m;
-        let ret: u32;
-        let pc = m_u32.count_ones();
-        match pc {
-            0 => {
-                ret = 0;
-            }
-            1 => {
-                ret = ((v_u32 & m_u32) as u32 != 0) as u32;
-            }
-            2 => {
-                let msb = _bextr_u32(v_u32, 31 - _lzcnt_u32(m_u32), 1);
-                let lsb = _bextr_u32(v_u32, _tzcnt_u32(m_u32), 1);
-                ret = (msb << 1) | lsb;
-            }
-            3 => {
-                let lz = 31 - _lzcnt_u32(m_u32);
-                let tz = _tzcnt_u32(m_u32);
-                let msb = _bextr_u32(v_u32, lz, 1);
-                let lsb = _bextr_u32(v_u32, tz, 1);
-                let m_u32_2 = _blsr_u32(m_u32);
-                let csb = _bextr_u32(v_u32, _tzcnt_u32(m_u32_2), 1);
-                ret = (msb << 2) | (csb << 1) | lsb;
-            }
-            4 => {
-                let lz = 31 - _lzcnt_u32(m_u32);
-                let tz = _tzcnt_u32(m_u32);
-                let msb1 = _bextr_u32(v_u32, lz, 1);
-                let lsb1 = _bextr_u32(v_u32, tz, 1);
-                let m_u32_2 = (m & (!((1 << lz) | (1 << tz)))) as u32;
-                let msb0 = _bextr_u32(v_u32, 31 - _lzcnt_u32(m_u32_2), 1);
-                let lsb0 = _bextr_u32(v_u32, _tzcnt_u32(m_u32_2), 1);
-                ret = (msb1 << 3) | (msb0 << 2) | (lsb0 << 1) | lsb1;
-            }
-            _ => {
-                let mut mm = _mm_cvtsi32_si128(!m_u32 as i32);
-                let mtwo = _mm_set1_epi64x((!0u64 - 1) as i64);
-                let mut clmul = _mm_clmulepi64_si128(mm, mtwo, 0);
-                let bit0 = _mm_cvtsi128_si32(clmul) as u32;
-                let mut a = v_u32 & m_u32;
-                a = (!bit0 & a) | ((bit0 & a) >> 1);
-                mm = _mm_and_si128(mm, clmul);
-                clmul = _mm_clmulepi64_si128(mm, mtwo, 0);
-                let bit1 = _mm_cvtsi128_si32(clmul) as u32;
-                a = (!bit1 & a) | ((bit1 & a) >> 2);
-                mm = _mm_and_si128(mm, clmul);
-                clmul = _mm_clmulepi64_si128(mm, mtwo, 0);
-                let bit2 = _mm_cvtsi128_si32(clmul) as u32;
-                a = (!bit2 & a) | ((bit2 & a) >> 4);
-                mm = _mm_and_si128(mm, clmul);
-                clmul = _mm_clmulepi64_si128(mm, mtwo, 0);
-                let bit3 = _mm_cvtsi128_si32(clmul) as u32;
-                ret = ((!bit3 & a) | ((bit3 & a) >> 8)) as u32;
-            }
-        }
-        return ret;
-    }
-}
-
 pub fn pext16_naive(input: u16, mut mask: u16) -> u16 {
     let mut result: u16 = 0;
     let mut bb: u16 = 1;
