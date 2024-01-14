bff6527114c1b995fa93a632b3c34b99fcc5a43a
YoungwoongLee <9902abcd@gmail.com>
2024-01-14 15:12:27 +0900

    Refactor util.rs

--- src/puyo-core/src/util.rs
+++ src/puyo-core/src/util.rs
@@ -1,5 +1,5 @@
 #[cfg(target_arch = "x86_64")]
-use std::arch::x86_64::*; 
+use std::arch::x86_64::*;
 use std::simd::{u16x8, u8x16};
 
 pub fn pext16(input: u16, mask: u16) -> u16 {
@@ -73,28 +73,22 @@ pub fn pext16_naive(input: u16, mut mask: u16) -> u16 {
 }
 
 pub fn mm_srli_si128_1(data_u16x8: u16x8) -> u16x8 {
-    unsafe{
-            let data = std::mem::transmute::<u16x8, u8x16>(data_u16x8);
-            return std::mem::transmute::<u8x16, u16x8>(
-                u8x16::from_array([
-                    data[1], data[2], data[3], data[4], data[5], data[6], data[7],
-                    data[8], data[9], data[10], data[11], data[12], data[13], data[14],
-                    data[15], 0,
-                ])
-            );
+    unsafe {
+        let data = std::mem::transmute::<u16x8, u8x16>(data_u16x8);
+        return std::mem::transmute::<u8x16, u16x8>(u8x16::from_array([
+            data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9],
+            data[10], data[11], data[12], data[13], data[14], data[15], 0,
+        ]));
     }
 }
 
 pub fn mm_srli_si128_2(data_u16x8: u16x8) -> u16x8 {
-        unsafe{
-            let data = std::mem::transmute::<u16x8, u8x16>(data_u16x8);
-            return std::mem::transmute::<u8x16, u16x8>(
-                u8x16::from_array([
-                    data[2], data[3], data[4], data[5], data[6], data[7],
-                    data[8], data[9], data[10], data[11], data[12], data[13], data[14],
-                    data[15], 0, 0
-                ])
-            );
+    unsafe {
+        let data = std::mem::transmute::<u16x8, u8x16>(data_u16x8);
+        return std::mem::transmute::<u8x16, u16x8>(u8x16::from_array([
+            data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10],
+            data[11], data[12], data[13], data[14], data[15], 0, 0,
+        ]));
     }
 }
 pub fn mm_srli_epi16_1(data_u16x8: u16x8) -> u16x8 {
@@ -106,25 +100,21 @@ pub fn mm_slli_epi16_1(data_u16x8: u16x8) -> u16x8 {
 }
 
 pub fn mm_slli_si128_1(data_u16x8: u16x8) -> u16x8 {
-        unsafe{
-            let data = std::mem::transmute::<u16x8, u8x16>(data_u16x8);
-            return std::mem::transmute::<u8x16, u16x8>(
-                u8x16::from_array([
-                    0, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
-                    data[8], data[9], data[10], data[11], data[12], data[13], data[14],
-                ])
-            );
+    unsafe {
+        let data = std::mem::transmute::<u16x8, u8x16>(data_u16x8);
+        return std::mem::transmute::<u8x16, u16x8>(u8x16::from_array([
+            0, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8],
+            data[9], data[10], data[11], data[12], data[13], data[14],
+        ]));
     }
 }
 
 pub fn mm_slli_si128_2(data_u16x8: u16x8) -> u16x8 {
-        unsafe{
-            let data = std::mem::transmute::<u16x8, u8x16>(data_u16x8);
-            return std::mem::transmute::<u8x16, u16x8>(
-                u8x16::from_array([
-                    0, 0, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
-                    data[8], data[9], data[10], data[11], data[12], data[13]
-                ])
-            );
+    unsafe {
+        let data = std::mem::transmute::<u16x8, u8x16>(data_u16x8);
+        return std::mem::transmute::<u8x16, u16x8>(u8x16::from_array([
+            0, 0, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8],
+            data[9], data[10], data[11], data[12], data[13],
+        ]));
     }
 }
